26738cf1e576112d6453ee085d05624fcc6b560e
Bedalbekov Bekulan <bedalbeka481@gmail.com>
2022-11-15 13:59:05 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        if (info(21,12))
+            System.out.println("Можно идти гулять");
+        if (info(15,9))
+            System.out.println("Можно идти гулять");
+        if (info(47,30))
+            System.out.println("Оставайтесь дома");
+        if (info(34,19))
+            System.out.println("Можно идти гулять");
+        if (info(54,27))
+            System.out.println("Оставайтесь дома");
+
+    }
+
+    public static boolean info(int age, int temperatre){
+        return true;
+    }
+
+}
+
